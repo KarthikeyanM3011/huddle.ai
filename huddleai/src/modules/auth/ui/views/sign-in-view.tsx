8fc49670955d2541185025ec410c63f3fd18da68
Karthikeyan <karthikeyanmjnk13579@gmail.com>
2025-08-05 13:01:35 +0530

    simple test ui

---
+++ huddleai/src/modules/auth/ui/views/sign-in-view.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export const SignInView = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+                <h2 className="text-2xl font-bold mb-6">Sign In</h2>
+            </div>
+        </div>
+    );
+}
---
+++ huddleai/src/modules/auth/ui/views/sign-up-view.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export const SignUpView = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+                <h2 className="text-2xl font-bold mb-6">Sign Up</h2>
+            </div>
+        </div>
+    );
+}
